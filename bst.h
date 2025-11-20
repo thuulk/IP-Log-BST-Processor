8ada33c726670529b263a471898f56fd849b4ca0
Diego <diegoreyesbarraza@hotmail.com>
2025-11-20 15:35:27 -0700

    implementing insertIp & reverseInOrder methods

--- bst.h
+++ bst.h
@@ -9,16 +9,25 @@ class BST {
     // ============= attributes =============
     Node* root;
 
-    Node* insert(Node* root, const size_t& key, const std::string& ip) {
+    // ============= private methods =============
+    Node* insertIp(Node* root, const size_t& key, const std::string& ip) { // Inserting recursively the new ip into its node by accessNum
         if (!root) return nullptr;
-        else if (key > root->accessNum) insert(root->right, key, ip);
-        else if (key < root->accessNum) insert(root->left, key, ip);
+        else if (key > root->accessNum) insertIp(root->right, key, ip);
+        else if (key < root->accessNum) insertIp(root->left, key, ip);
         else root->ips.push_back(ip);
         return root;
     }
 
+    void reverseInOrder(Node* root) const { // Printing ips recursively in a greatest-to-less order
+       if (!root) return;
+       reverseInOrder(root->right);
+       std::cout << "IPs spotted " << root->accessNum << " times:" << std::endl; 
+       for (std::string& ip : root->ips) std::cout << ip << std::endl;
+       std::cout << std::endl;
+    }
+
     public:
-    // ============= public methods =============
+    // ============= encapsulating methods =============
     BST() : root(nullptr) {}
 
 
