a3b2c3d821d4139e818c3f143f1109454e1f548d
Diego <diegoreyesbarraza@hotmail.com>
2025-11-21 10:56:33 -0700

    final implementation: main.cpp

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "bst.h"
+#include <algorithm>
+
+int main() {
+
+    std::string filename;
+    std::cout << "ingresa el nombre del archivo a importar: ";
+    std::cin >> filename;
+
+    try {
+        Importation imp;
+        auto ipCount = imp.loadLogs(filename);
+
+        if (ipCount.empty()) {
+            std::cout << "El archivo no se importo correctamente" << std::endl;
+            return 0;
+        }
+
+        BST tree;
+
+        for (const auto& par : ipCount) {
+            const std::string& ip = par.first;
+            const size_t& accessNum = par.second;
+            tree.insert(accessNum, ip);
+        }
+
+        size_t k = 5;
+        auto topMap = tree.topTier(k);               // unordered_map<ip, accesos>
+
+        if (topMap.empty()) {
+            std::cout << "No se encontraron IPs en la bitácora.\n";
+            return 0;
+        }
+
+        // Pasar el unordered_map a un vector para poder ordenarlo de mayor a menor
+        std::vector<std::pair<std::string, std::size_t>> topVec(
+            topMap.begin(), topMap.end()
+        );
+
+        std::sort(topVec.begin(), topVec.end(),
+                  [](const auto& a, const auto& b) {
+                      return a.second > b.second; // ordenar por accesos descendente
+                  });
+
+        // 4) Imprimir las 5 IPs con más accesos (o menos si no hay tantas)
+        std::cout << "\n===== Top 5 IPs con mas accesos =====\n";
+        std::size_t limite = std::min<std::size_t>(5, topVec.size());
+
+        for (std::size_t i = 0; i < limite; ++i) {
+            std::cout << i + 1 << ". "
+                      << topVec[i].first << " -> "
+                      << topVec[i].second << " accesos\n";
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error al procesar la bitácora: " << e.what() << '\n';
+        return 1;
+    }
+    
+
+    
+
+
+
+    return 0;
+}
